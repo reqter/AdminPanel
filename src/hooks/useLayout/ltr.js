c74389bfd7de2dcb99788f80e09a8df509d6db53
saeedPadyab <padyabsaeed@gmail.com>
2019-06-09 10:06:05 +0300

    request description  in view  and create

--- src/hooks/useLayout/ltr.js
+++ src/hooks/useLayout/ltr.js
@@ -8,6 +8,7 @@ export default {
   fontFamily: 'OpenSance',
   textAlign: 'left',
   rowDirection: 'row',
+  direction: 'ltr',
   'mp-products-list-padding': '0 0 0 30px',
   products_list_filters_margin: '0 5px 0 0 '
 }
